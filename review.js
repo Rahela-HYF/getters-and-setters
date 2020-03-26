6c5a5cf9599b9e8ed0dc22abd4d751f20fbb7933
Rahal Semu <59530968+Rahela-HYF@users.noreply.github.com>
2020-03-26 23:21:54 +0100

    Update review.js

--- review.js
+++ review.js
@@ -1,5 +1,5 @@
 const CONFIG = {
-  user: 'hackyourfuturebelgium',
+  user: 'Rahela-HYF',
   repo: 'functions-to-methods',
   root: process.argv[2] || './',
   ignore: ['.git', 'review.js', 'node_modules'],
